f2cdcae471dc906458bbeb88444bbbfb1183c31b
Washiil <65116802+Washiil@users.noreply.github.com>
2023-08-31 15:50:05 -0400

    added answers to a question and fixed colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,80 +2,63 @@
 	import '../app.postcss';
 	import { goto } from '$app/navigation';
 	import { redirect } from '@sveltejs/kit';
-	import { AppShell, AppBar, TreeView, TreeViewItem, type TreeViewNode } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, TreeView, TreeViewItem, type TreeViewNode, LightSwitch } from '@skeletonlabs/skeleton';
 
 	let questions: Record<string, number[]> = {
-		'2021': [1],
-		'2022': [1, 2, 3, 4],
+			'2021': [1],
+			'2022': [1, 2, 3, 4],
 	}
 </script>
 
 <!-- App Shell -->
-<AppShell>
+<AppShell slotHeader='p-2 rounded'>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">CSA-Savior</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-
-		
+			<!-- App Bar -->
+			<AppBar background='variant-soft-tertiary'>
+					<svelte:fragment slot="lead">
+							<strong class="text-xl uppercase">CSA-Savior</strong>
+					</svelte:fragment>
+					<svelte:fragment slot="trail">
+							<a
+									class="btn btn-sm variant-ghost-surface"
+									href="https://github.com/skeletonlabs/skeleton"
+									target="blank"
+									rel="noreferrer"
+							>
+									GitHub
+							</a>
+							<LightSwitch />
+					</svelte:fragment>
+			</AppBar>
 	</svelte:fragment>
 	<!-- This is the sidebar -->
 	<svelte:fragment slot="sidebarLeft">
-		<!-- Hidden below Tailwind's large breakpoint -->
-		<div id="sidebar-left" class="hidden lg:block bg-surface-800 m-2 rounded w-48 no-scrollbar" style='height: 98%'>
-			<TreeView>
-				{#each Object.entries(questions) as [key, value]}
-					<TreeViewItem>
-					{key}
-					<svelte:fragment slot="children">
-						{#each questions[key] as question}
-							<TreeViewItem>
-								<button on:click={() => goto(`/${key}/Question_${question}`)}>Question {question}</button>
-							</TreeViewItem>
-						{/each}
-					</svelte:fragment>
-					</TreeViewItem>
-				{/each}
-			</TreeView>
-		</div>
+			<!-- Hidden below Tailwind's large breakpoint -->
+			<div id="sidebar-left" class="hidden lg:block variant-soft-tertiary m-2 rounded w-48 no-scrollbar" style='height: 98%'>
+					<TreeView>
+							{#each Object.entries(questions) as [key, value]}
+									<TreeViewItem>
+									{key}
+									<svelte:fragment slot="children">
+											{#each questions[key] as question}
+													<TreeViewItem on:click={() => goto(`/${key}/Question_${question}`)}>
+															Question {question}
+													</TreeViewItem>
+											{/each}
+									</svelte:fragment>
+									</TreeViewItem>
+							{/each}
+					</TreeView>
+			</div>
 	</svelte:fragment>
 	<!-- Page Route Content -->
-	<div class='bg-surface-800 m-2 rounded p-4' style='height: 98%'>
-		<slot />
+	<div class='variant-soft-tertiary m-2 rounded p-4' style='height: 98%'>
+			<slot />
 	</div>
 </AppShell>
 
 <style>
 .no-scrollbar::-webkit-scrollbar {
-    display: none;
+	display: none;
 }
 </style>
--- src/routes/2022/Question_1/+page.svelte
+++ src/routes/2022/Question_1/+page.svelte
@@ -7,14 +7,40 @@
 </script>
 
 <div>
-	<h1>Question 1</h1>
+	<h1>Question 1: Methods and Control Structures</h1>
 	<br>
 	<h2>Part A</h2>
+	<br>
 	<CodeBlock language="java" code={`
-System.out.println("Hello world");
-
-for(String i : strings) {
-
-}`}>
-</CodeBlock>
+public int getScore() {
+    int score = 0;
+    if (levelOne.goalReached()) {
+        score = levelOne.getPoints();
+        if (levelTwo.goalReached()) {
+            score += levelTwo.getPoints();
+            if (levelThree.goalReached()) {
+                score += levelThree.getPoints();
+            }
+        }
+    }
+    if (isBonus()) {
+        score *= 3;
+    }
+    return score;
+}`}/>
+	<br>
+	<h2>Part B</h2>
+	<br>
+	<CodeBlock language="java" code={`
+public int playManyTimes(int num) {
+    int max = 0;
+    for (int i = 0; i < num; i++) {
+        play();
+        int score = getScore();
+        if (score > max) {
+            max = score;
+        }
+    }
+    return max;
+}`}/>
 </div>
